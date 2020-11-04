75079598ecb9096c9714e537e0b26bfbd86e8dbd
bdijo <bdijo@D-IMEC-TRAINEE->
2020-11-04 08:16:01 +0200

    Added program execution logic to Main in Program

--- SimpleWebScraper/Program.cs
+++ SimpleWebScraper/Program.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using SimpleWebScraper.Builders;
+using SimpleWebScraper.Data;
+using SimpleWebScraper.Workers;
+using System;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace SimpleWebScraper
 {
     class Program
     {
+        private const string Method = "search";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter which city you would like to scrape information from: ");
+            var craigslistCity = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine("Please enter CraigsList category that you would like to scrape: ");
+            var craigslistCategoryName = Console.ReadLine() ?? string.Empty;
+
+            using (WebClient client = new WebClient())
+            {
+                string content = client.DownloadString($"http://{craigslistCity.Replace(" ", string.Empty)}.craigslist.org/{Method}/{craigslistCategoryName}");
+
+                ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                    .WithData(content)
+                    .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                    .WithRegexOption(RegexOptions.ExplicitCapture)
+                    .WithPart(new ScrapeCriteriaPartBuilder()
+                        .WithRegex(@">(.*?)</a>")
+                        .WithRegexOption(RegexOptions.Singleline)
+                        .Build())
+                    .WithPart(new ScrapeCriteriaPartBuilder()
+                        .WithRegex(@"href=\""(.*?)\""")
+                        .WithRegexOption(RegexOptions.Singleline)
+                        .Build())
+                    .Build();
+
+                Scraper scraper = new Scraper();
+
+                var scrapedElements = scraper.Scrape(scrapeCriteria);
+
+                if (scrapedElements.Any())
+                {
+                    foreach (var scrapedElement in scrapedElements) Console.WriteLine(scrapedElement);
+                }
+                else
+                {
+                    Console.WriteLine("There were no matches for the specified scrape criteria. ");
+                }
+            }
         }
     }
 }
